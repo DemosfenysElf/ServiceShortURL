7ed0aa10e2bf911990cc8cafb3c32dd0074440ca
DemosfenysElf <109169965+DemosfenysElf@users.noreply.github.com>
2022-12-16 13:30:22 +0300

    Increment 6

--- internal/shorturlservice/shortURLService.go
+++ internal/shorturlservice/shortURLService.go
@@ -1,23 +1,54 @@
 package shorturlservice
 
-import "math/rand"
+import (
+	"log"
+	"math/rand"
+)
 
 var urlmap = make(map[string]string)
+var fileName = "shortsURl.log"
 
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func GetURL(short string) (url string) {
+
+	cons, err := NewConsumer(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cons.Close()
+
+	for {
+		readURL, err := cons.ReadURL()
+		if err != nil {
+			break
+		}
+		if readURL.ShortURL == short {
+			return urlmap[readURL.ShortURL]
+		}
+	}
+
 	return urlmap[short]
 }
 
 func SetURL(url string) (short string) {
 	short = shortURL()
-
 	for _, ok := urlmap[short]; ok; {
 		short = shortURL()
 	}
 	urlmap[short] = url
+	////////// дублирование в файл
+	urli := &URLInfo{URL: url, ShortURL: short}
 
+	//defer os.Remove(fileName)
+	prod, err := NewProducer(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer prod.Close()
+	if err := prod.WriteURL(urli); err != nil {
+		log.Fatal(err)
+	}
 	return
 }
 
