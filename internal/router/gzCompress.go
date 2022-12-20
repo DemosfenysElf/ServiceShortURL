a47a4dd7b83e824a40feded998ff28a0f29d4b28
DemosfenysElf <109169965+DemosfenysElf@users.noreply.github.com>
2022-12-20 19:20:48 +0300

    Increment 8

--- internal/router/gzCompress.go
+++ internal/router/gzCompress.go
@@ -11,16 +11,19 @@ func serviceCompress(data []byte) ([]byte, error) {
 
 	w, err := flate.NewWriter(&b, flate.BestCompression)
 	if err != nil {
+		fmt.Println(">>>>>>>>>>A_5")
 		return nil, fmt.Errorf("failed init compress writer: %v", err)
 	}
 
 	_, err = w.Write(data)
 	if err != nil {
+		fmt.Println(">>>>>>>>>>A_6")
 		return nil, fmt.Errorf("failed write data to compress temporary buffer: %v", err)
 	}
 
 	err = w.Close()
 	if err != nil {
+		fmt.Println(">>>>>>>>>>A_7")
 		return nil, fmt.Errorf("failed compress data: %v", err)
 	}
 
