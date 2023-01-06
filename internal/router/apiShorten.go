eb726f2a638fffeaf1dfbe46750d46f4e629c154
DemosfenysElf <109169965+DemosfenysElf@users.noreply.github.com>
2023-01-07 01:39:08 +0300

    Increment 9

--- internal/router/apiShorten.go
+++ internal/router/apiShorten.go
@@ -37,6 +37,8 @@ func (s *Server) APIShorten(c echo.Context) error {
 		return fmt.Errorf("marshal error")
 	}
 
+	fmt.Println("<<<APIShorten>>>>>> URL: ", string(body), " | shortUrl: ", short)
+
 	if c.Request().Header.Get("Accept-Encoding") == "gzip" {
 		shortU, err = serviceCompress(shortU)
 		if err != nil {
