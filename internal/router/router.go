4d22f7ba424084810f8d30c9a978c79e3f108312
DemosfenysElf <109169965+DemosfenysElf@users.noreply.github.com>
2023-01-07 20:10:27 +0300

    Increment 9

--- internal/router/router.go
+++ internal/router/router.go
@@ -6,23 +6,23 @@ import (
 	"github.com/labstack/echo"
 	"io"
 	"log"
-	"os"
 )
 
 type ConfigURL struct {
 	ServerAddress string `env:"SERVER_ADDRESS"`
 	BaseURL       string `env:"BASE_URL"`
 	Storage       string `env:"FILE_STORAGE_PATH"`
+	ConnectDB     string `env:"DATABASE_DSN"`
 }
 
 type Server struct {
 	Cfg    ConfigURL
 	Serv   *echo.Echo
 	Writer io.Writer
+	DB     DatabaseInterface
 }
 
 func (s *Server) Router() error {
-	os.Remove(s.Cfg.Storage)
 
 	errConfig := env.Parse(&s.Cfg)
 	if errConfig != nil {
@@ -38,8 +38,23 @@ func (s *Server) Router() error {
 	if s.Cfg.Storage == "" {
 		flag.StringVar(&s.Cfg.Storage, "f", "shortsURl.log", "New FILE_STORAGE_PATH")
 	}
+	if s.Cfg.ConnectDB == "" {
+		flag.StringVar(&s.Cfg.Storage, "d", "postgres://postgres:0000@localhost:5432/postgres", "New DATABASE_DSN")
+	}
 	flag.Parse()
 
+	// DB connection
+	var err error
+	s.DB, err = InitDB()
+	if err != nil {
+		return err
+	}
+	if err := s.DB.Connect(s.Cfg.ConnectDB); err != nil {
+
+		return err
+	}
+	defer s.DB.Close()
+	//
 	e := echo.New()
 
 	e.Use(s.gzipHandle)
@@ -50,6 +65,7 @@ func (s *Server) Router() error {
 	e.POST("/", s.PostURLToShort)
 	e.POST("/api/shorten", s.APIShorten)
 	e.GET("/api/user/urls", s.APIUserURL)
+	e.GET("/ping", s.PingBD)
 
 	errStart := e.Start(s.Cfg.ServerAddress)
 
