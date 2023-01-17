d293f6aa6a78d04053c976165c60f1afa235e2db
DemosfenysElf <109169965+DemosfenysElf@users.noreply.github.com>
2023-01-17 17:29:03 +0300

    Increment 13

--- internal/router/pingDB.go
+++ internal/router/pingDB.go
@@ -16,7 +16,7 @@ func (s *serverShortener) PingDB(c echo.Context) error {
 		c.Response().WriteHeader(http.StatusInternalServerError)
 		return nil
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 	if err := s.DB.Ping(ctx); err != nil {
 		c.Response().WriteHeader(http.StatusInternalServerError)
--- internal/shorturlservice/databaseInterface.go
+++ internal/shorturlservice/databaseInterface.go
@@ -41,7 +41,7 @@ func (db *Database) Connect(connStr string) (err error) {
 		return err
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 	err = db.Ping(ctx)
 	if err != nil {
