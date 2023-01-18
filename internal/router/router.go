fc5a765d84bb465be7c1ad4a055b47ae428517e7
DemosfenysElf <109169965+DemosfenysElf@users.noreply.github.com>
2023-01-18 20:05:53 +0300

    Increment 14

--- internal/router/router.go
+++ internal/router/router.go
@@ -100,8 +100,8 @@ func (s *serverShortener) InitRouter() {
 	}
 	flag.Parse()
 
-	//s.Cfg.Storage = ""
-	//s.Cfg.ConnectDB = ""
+	s.Cfg.Storage = ""
+	s.Cfg.ConnectDB = ""
 
 	if err := s.startBD(); err == nil {
 		fmt.Println(">>>>use BD<<<<", s.Cfg.ConnectDB)
