127bcef80013ef3cc2a4dd983cf90e924247ae79
Demosfenys <Demosfenys@yandex.ru>
2023-03-27 17:05:30 +0300

    iter20_tests3

--- internal/router/handlerDB_test.go
+++ internal/router/handlerDB_test.go
@@ -1,7 +1,9 @@
 package router
 
 import (
+	"database/sql/driver"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -10,6 +12,8 @@ import (
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/labstack/echo"
 
 	"ServiceShortURL/internal/shorturlservice"
@@ -30,6 +34,7 @@ func TestPostDB(t *testing.T) {
 		urlResult      []string
 		urllist        []string
 		bodyURLJSON    string
+		result         driver.Result
 	}{
 		{
 			name: "TestBDApiShortenBatch1",
@@ -56,6 +61,7 @@ func TestPostDB(t *testing.T) {
 							{"correlation_id":"baseurl2","original_url":"https://metanit.com/"},
 							{"correlation_id":"baseurl3","original_url":"https://otion.site/context-19ec3dbb0cd4ec"},
 							{"correlation_id":"baseurl4","original_url":"https://stepik.org/lesson/359395"}]`,
+			result: sqlmock.NewResult(1, 1),
 		},
 		{
 			name: "TestBDApiShortenBatch2",
@@ -82,6 +88,7 @@ func TestPostDB(t *testing.T) {
 							{"correlation_id":"baseurl2","original_url":"https://metanit.com/"},
 							{"correlation_id":"baseurl3","original_url":"https://otion.site/context-19ec3dbb0cd4ec"},
 							{"correlation_id":"baseurl4","original_url":"https://stepik.org/lesson/359395"}`,
+			result: sqlmock.NewResult(1, 1),
 		},
 		{
 			name: "TestBDApiShortenBatch3",
@@ -108,12 +115,12 @@ func TestPostDB(t *testing.T) {
 							{"correlation_id":"baseurl2","original_url":"https://metanit.com/"},
 							{"correlation_id":"baseurl3","original_url":"https://metanit.com/"},
 							{"correlation_id":"baseurl4","original_url":"https://stepik.org/lesson/359395"}]`,
+			result: sqlmock.NewResult(1, 1),
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
 			db, mock, err := sqlmock.New()
 			if err != nil {
 				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
@@ -132,10 +139,9 @@ func TestPostDB(t *testing.T) {
 			for i := range tt.generatorShort.Result {
 				mock.ExpectExec("insert into ShortenerURL").
 					WithArgs(tt.urllist[i], tt.generatorShort.ShortURL(), user.Name, user.Value, false).
-					WillReturnResult(sqlmock.NewResult(1, 1))
+					WillReturnResult(tt.result)
 			}
 			tt.generatorShort.Index = 0
-
 			//
 
 			e := echo.New()
@@ -275,6 +281,250 @@ func TestDBGet(t *testing.T) {
 					t.Errorf("Ожидаемое значение %s, полученное %s", tt.want.url, resBody)
 				}
 
+			}
+
+		})
+	}
+}
+
+func TestDBDelete(t *testing.T) {
+
+	type want struct {
+		codeDel  int
+		response string
+	}
+	tests := []struct {
+		name           string
+		want           want
+		wantErr        bool
+		generatorShort *shorturlservice.TestGenerator
+		generatorUser  *shorturlservice.TestGeneratorUser
+		bodyURLJSON    string
+	}{
+		{
+			name: "TestBDDel",
+			want: want{
+				codeDel:  202,
+				response: `{"status":"ok"}`,
+			},
+			generatorShort: &shorturlservice.TestGenerator{Result: []string{"Short1", "Short2", "Short3", "Short4"}, Index: 0},
+			generatorUser:  &shorturlservice.TestGeneratorUser{Result: []byte("UserUserUserUser")},
+
+			bodyURLJSON: `["/Short1","/Short2","/Short3","/Short4"]`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			db, mock, err := sqlmock.New()
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+			}
+
+			rout := InitTestServer()
+			rout.Cfg.Storage = testStorageURL
+			mockDB := &shorturlservice.Database{RandomShort: tt.generatorShort}
+
+			mockDB.SetConnection(db)
+			rout.DB = mockDB
+			rout.StorageInterface = mockDB
+			rout.GeneratorUsers = tt.generatorUser
+			user := rout.kykiDB()
+			//
+			for range tt.generatorShort.Result {
+				mock.ExpectExec("UPDATE ShortenerURL SET deleted = true WHERE short=? AND valueUser=?").
+					WithArgs(tt.generatorShort.ShortURL(), user.Value).
+					WillReturnResult(sqlmock.NewResult(1, 1))
+			}
+			tt.generatorShort.Index = 0
+			//
+
+			e := echo.New()
+			request := httptest.NewRequest(http.MethodDelete, "/api/user/urls", strings.NewReader(tt.bodyURLJSON))
+
+			request.AddCookie(user)
+			responseRecorder := httptest.NewRecorder()
+			c := e.NewContext(request, responseRecorder)
+
+			rout.Serv = e
+			rout.DeleteAPIUserURLs(c)
+			response := responseRecorder.Result()
+			defer response.Body.Close()
+
+			if response.StatusCode != tt.want.codeDel {
+				t.Errorf("Expected status code %d, got %d", tt.want.codeDel, responseRecorder.Code)
+			}
+
+		})
+	}
+}
+
+func TestDBUniqueViolation(t *testing.T) {
+
+	type want struct {
+		codePost int
+		response string
+	}
+	tests := []struct {
+		name           string
+		want           want
+		wantErr        bool
+		generatorShort *shorturlservice.TestGenerator
+		generatorUser  *shorturlservice.TestGeneratorUser
+		urlResult      string
+		urllist        string
+		bodyURL        string
+		errUnique      *pgconn.PgError
+	}{
+		{
+			name: "TestBDApiShortenBatch1",
+			want: want{
+				codePost: http.StatusConflict,
+				response: `{"status":"ok"}`,
+			},
+			generatorShort: &shorturlservice.TestGenerator{Result: []string{"Short1"}, Index: 0},
+			generatorUser:  &shorturlservice.TestGeneratorUser{Result: []byte("UserUserUserUser")},
+			urlResult:      "Short1",
+			urllist:        "https://www.youtube.com/watch?v=UK7yzgVpnDA",
+			bodyURL:        "https://www.youtube.com/watch?v=UK7yzgVpnDA",
+			errUnique:      &pgconn.PgError{Code: pgerrcode.UniqueViolation},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			db, mock, err := sqlmock.New()
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+			}
+			defer db.Close()
+			rout := InitTestServer()
+			rout.Cfg.Storage = testStorageURL
+			mockDB := &shorturlservice.Database{RandomShort: tt.generatorShort}
+
+			mockDB.SetConnection(db)
+			rout.DB = mockDB
+			rout.StorageInterface = mockDB
+			rout.GeneratorUsers = tt.generatorUser
+			user := rout.kykiDB()
+			//
+			mock.ExpectExec("insert into ShortenerURL").
+				WithArgs(tt.urllist, tt.generatorShort.ShortURL(), user.Name, user.Value, false).
+				WillReturnError(tt.errUnique)
+
+			//db.connection.QueryRowContext(ctx, "select short from ShortenerURL where url = $1", url)
+			row := sqlmock.NewRows([]string{"short"}).
+				AddRow(tt.urlResult)
+
+			mock.ExpectQuery("select short from ShortenerURL where ?").
+				WithArgs(tt.urllist).
+				WillReturnRows(row)
+
+			tt.generatorShort.Index = 0
+			//
+
+			e := echo.New()
+			request := httptest.NewRequest(http.MethodPost, "/api/shorten/batch", strings.NewReader(tt.bodyURL))
+
+			request.AddCookie(user)
+			responseRecorder := httptest.NewRecorder()
+			c := e.NewContext(request, responseRecorder)
+
+			rout.Serv = e
+			rout.PostURLToShort(c)
+			response := responseRecorder.Result()
+
+			defer response.Body.Close()
+			resBody, err := io.ReadAll(response.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if response.StatusCode != tt.want.codePost {
+				t.Errorf("Expected status code %d, got %d", tt.want.codePost, responseRecorder.Code)
+			}
+
+			if string(resBody[1:]) != tt.urlResult {
+				t.Errorf("Короткая ссылка не соответствует ожидаемой")
+			}
+			fmt.Println(string(resBody))
+
+		})
+	}
+}
+
+func TestDBPing(t *testing.T) {
+
+	type want struct {
+		codeGet  int
+		url      string
+		response string
+	}
+	tests := []struct {
+		name           string
+		want           want
+		generatorShort *shorturlservice.TestGenerator
+		generatorUser  *shorturlservice.TestGeneratorUser
+		errPing        error
+	}{
+		{
+			name: "TestDBGetPing1",
+			want: want{
+				codeGet:  http.StatusOK,
+				response: `{"status":"ok"}`,
+			},
+			generatorShort: &shorturlservice.TestGenerator{Result: []string{"Short1"}, Index: 0},
+			generatorUser:  &shorturlservice.TestGeneratorUser{Result: []byte("UserUserUserUser")},
+			errPing:        nil,
+		},
+		{
+			name: "TestDBGetPing2",
+			want: want{
+				codeGet:  http.StatusInternalServerError,
+				response: `{"status":"ok"}`,
+			},
+			generatorShort: &shorturlservice.TestGenerator{Result: []string{"Short1"}, Index: 0},
+			generatorUser:  &shorturlservice.TestGeneratorUser{Result: []byte("UserUserUserUser")},
+			errPing:        errors.New("Ping fail"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			db, mock, err := sqlmock.New(sqlmock.MonitorPingsOption(true))
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+			}
+			defer db.Close()
+			rout := InitTestServer()
+			rout.Cfg.Storage = testStorageURL
+			mockDB := &shorturlservice.Database{RandomShort: tt.generatorShort}
+
+			mockDB.SetConnection(db)
+			rout.DB = mockDB
+			rout.StorageInterface = mockDB
+			rout.GeneratorUsers = tt.generatorUser
+			user := rout.kykiDB()
+			//
+			mock.ExpectPing().WillReturnError(tt.errPing)
+			//
+			e := echo.New()
+			request := httptest.NewRequest(http.MethodGet, "/ping", nil)
+
+			request.AddCookie(user)
+			responseRecorder := httptest.NewRecorder()
+			c := e.NewContext(request, responseRecorder)
+
+			rout.Serv = e
+			rout.GetPingDB(c)
+			response := responseRecorder.Result()
+			defer response.Body.Close()
+
+			if response.StatusCode != tt.want.codeGet {
+				t.Errorf("Expected status code %d, got %d", tt.want.codeGet, responseRecorder.Code)
 			}
 
 		})
--- internal/router/test_test.go
+++ internal/router/test_test.go
@@ -1,9 +1,9 @@
 package router
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -12,41 +12,50 @@ import (
 	"ServiceShortURL/internal/shorturlservice"
 )
 
-func TestDB11111(t *testing.T) {
+func TestDB111(t *testing.T) {
 
 	type want struct {
-		codeDel  int
+		codeGet  int
+		url      string
 		response string
 	}
 	tests := []struct {
 		name           string
 		want           want
-		wantErr        bool
 		generatorShort *shorturlservice.TestGenerator
 		generatorUser  *shorturlservice.TestGeneratorUser
-		bodyURLJSON    string
+		errPing        error
 	}{
 		{
-			name: "TestBDDel",
+			name: "TestDBGetPing1",
 			want: want{
-				codeDel:  202,
+				codeGet:  http.StatusOK,
 				response: `{"status":"ok"}`,
 			},
-			generatorShort: &shorturlservice.TestGenerator{Result: []string{"Short1", "Short2", "Short3", "Short4"}, Index: 0},
+			generatorShort: &shorturlservice.TestGenerator{Result: []string{"Short1"}, Index: 0},
 			generatorUser:  &shorturlservice.TestGeneratorUser{Result: []byte("UserUserUserUser")},
-
-			bodyURLJSON: `["/Short1","/Short2","/Short3","/Short4"]`,
+			errPing:        nil,
+		},
+		{
+			name: "TestDBGetPing2",
+			want: want{
+				codeGet:  http.StatusInternalServerError,
+				response: `{"status":"ok"}`,
+			},
+			generatorShort: &shorturlservice.TestGenerator{Result: []string{"Short1"}, Index: 0},
+			generatorUser:  &shorturlservice.TestGeneratorUser{Result: []byte("UserUserUserUser")},
+			errPing:        errors.New("Ping fail"),
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 
-			db, mock, err := sqlmock.New()
+			db, mock, err := sqlmock.New(sqlmock.MonitorPingsOption(true))
 			if err != nil {
 				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 			}
-
+			defer db.Close()
 			rout := InitTestServer()
 			rout.Cfg.Storage = testStorageURL
 			mockDB := &shorturlservice.Database{RandomShort: tt.generatorShort}
@@ -57,28 +66,22 @@ func TestDB11111(t *testing.T) {
 			rout.GeneratorUsers = tt.generatorUser
 			user := rout.kykiDB()
 			//
-			for range tt.generatorShort.Result {
-				mock.ExpectExec("UPDATE ShortenerURL SET deleted = true WHERE short=? AND valueUser=?").
-					WithArgs(tt.generatorShort.ShortURL(), user.Value).
-					WillReturnResult(sqlmock.NewResult(1, 1))
-			}
-			tt.generatorShort.Index = 0
+			mock.ExpectPing().WillReturnError(tt.errPing)
 			//
-
 			e := echo.New()
-			request := httptest.NewRequest(http.MethodDelete, "/api/user/urls", strings.NewReader(tt.bodyURLJSON))
+			request := httptest.NewRequest(http.MethodGet, "/ping", nil)
 
 			request.AddCookie(user)
 			responseRecorder := httptest.NewRecorder()
 			c := e.NewContext(request, responseRecorder)
 
 			rout.Serv = e
-			rout.DeleteAPIUserURLs(c)
+			rout.GetPingDB(c)
 			response := responseRecorder.Result()
 			defer response.Body.Close()
 
-			if response.StatusCode != tt.want.codeDel {
-				t.Errorf("Expected status code %d, got %d", tt.want.codeDel, responseRecorder.Code)
+			if response.StatusCode != tt.want.codeGet {
+				t.Errorf("Expected status code %d, got %d", tt.want.codeGet, responseRecorder.Code)
 			}
 
 		})
